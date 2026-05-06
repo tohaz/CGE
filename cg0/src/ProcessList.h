4d2c90d9a23720624973df02e27f7665ef6052ba
tohaz <35155382+tohaz@users.noreply.github.com>
2026-05-06 05:24:41 +0200

    missing files

---
+++ cg0/src/ProcessList.h
@@ -0,0 +1,17 @@
+#ifndef PROCESSLIST_H_
+#define PROCESSLIST_H_
+
+namespace cg {
+
+  class ProcessList {
+    public:
+      ProcessList();
+      virtual ~ProcessList();
+  };
+
+} /* namespace cg */
+
+
+
+
+#endif
